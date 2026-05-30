343714e0a8b897a7cdda99d969d50cb7048e66ff
Tad Reeves <tadreeves@gmail.com>
2026-05-30 08:43:15 -0400

    perf: defer New Relic load to delayed phase

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,6 +4,17 @@ import { sampleRUM } from './aem.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
+function loadScript(src) {
+	if (document.querySelector(`script[src="${src}"]`)) return;
+
+	const script = document.createElement('script');
+	script.src = src;
+	script.async = true;
+	document.head.append(script);
+}
+
+loadScript('/scripts/newrelic.js');
+
 // add more delayed functionality here
 /* Add in ALI ARMS RUM CODE */
 
